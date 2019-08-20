828dbc04718c778a5082cb186be5db6c0d53dfb3
dremdem <dremdem@gmail.com>
2019-08-20 12:40:10 +0300

    Add is_sync function to the parameter table Get gid of old parameter form

--- ahi/front/urls.py
+++ ahi/front/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import index, asset_tree, parameters, set_root_asset
+from .views import index, asset_tree, parameters, set_root_asset, set_sync
 
 urlpatterns = [
     path('', index, name='index'),
     path('asset_tree/<int:root_asset_id>', asset_tree, name='asset_tree'),
     path('set_root_asset', set_root_asset, name='set_root_asset'),
+    path('set_sync', set_sync, name='set_sync'),
     path('parameters', parameters, name='parameters'),
     # path(r'^select2/', include('django_select2.urls')),
 ]
--- ahi/front/views.py
+++ ahi/front/views.py
@@ -13,35 +13,20 @@ def index(request):
     """
     Starting page
     """
-
-    root_asset = None
-
-    if request.method == 'POST':
-        root_choice_form = RootChoiceForm(request.POST)
-        if root_choice_form.is_valid():
-            root_asset = root_choice_form.cleaned_data['root_asset']
-            is_sync_hierarchy = root_choice_form.cleaned_data['is_sync_hierarchy']
-            set_parameter_value('root_asset', 'I', root_asset)
-            set_parameter_value('is_sync_hierarchy', 'B', is_sync_hierarchy)
-            if is_sync_hierarchy:
-                sync_asset_hierarchy()
-    else:
-        is_sync_hierarchy = get_parameter_value('is_sync_hierarchy', default_value=True)
-        if is_sync_hierarchy:
-            sync_asset_hierarchy()
-        root_asset = get_parameter_value('root_asset', default_value=settings.DEFAULT_ASSET_ID)
-        root_choice_form = RootChoiceForm(
-            initial={'root_asset': root_asset, 'is_sync_hierarchy': is_sync_hierarchy})
+    is_sync_hierarchy = get_parameter_value('is_sync_hierarchy', default_value=True)
+    if is_sync_hierarchy:
+        sync_asset_hierarchy()
+    root_asset = get_parameter_value('root_asset', default_value=settings.DEFAULT_ASSET_ID)
 
     select2_form = RootAssetSelect2WidgetForm()
 
-    return render(request, 'index.html', {'form': root_choice_form, 'form_select2': select2_form, 'root_asset': root_asset})
+    return render(request, 'index.html', {'form_select2': select2_form,
+                                          'root_asset': root_asset})
 
 
 def asset_tree(request, root_asset_id=settings.DEFAULT_ASSET_ID):
     asset_hierarchy = get_local_asset_hierarchy(root_asset_id)
     json_tree = build_json_tree(asset_hierarchy, root_asset_id)
-
     return JsonResponse(json_tree)
 
 
@@ -52,4 +37,10 @@ def parameters(request):
 def set_root_asset(request):
     root_asset = Asset.objects.filter(pk=request.POST['asset_number'])[0].instance_id
     set_parameter_value('root_asset', 'I', root_asset)
+    return HttpResponse()
+
+
+def set_sync(request):
+    is_sync_hierarchy = True if request.POST.get('is_sync') == 'on' else False
+    set_parameter_value('is_sync_hierarchy', 'B', is_sync_hierarchy)
     return HttpResponse()
