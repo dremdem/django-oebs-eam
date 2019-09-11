15d93722722683f37da0cd9c9e6d47d1cce423b6
dremdem <dremdem@gmail.com>
2019-09-11 13:04:08 +0300

    just playing around)

--- ahi/oebs/tests/test_parameter_pytest.py
+++ ahi/oebs/tests/test_parameter_pytest.py
@@ -31,15 +31,27 @@ def get_asset_child(instance_id: int, instances_dict: list) -> list:
     asset = Asset.objects.get(instance_id=instance_id)
 
 
+def get_child_assets_id(asset_list: list, asset_id: int) -> list:
+
+    for child_asset in Asset.objects.get(pk=asset_id).asset_set.all():
+        if child_asset.asset_set.exists():
+            asset_list += get_child_assets_id(asset_list, child_asset.instance_id)
+        asset_list.append(child_asset.instance_id)
+
+    return asset_list
+
+
 def get_random_parent_asset(instance_id: int) -> int:
     """
     get random parent asset not linked to any child assets
     :param instance_id: asset instance_id
     """
 
-    # get the list of all child's instance_id
-    # child_instance_ids = Asset.objects.
-    pass
+    child_list = [instance_id]
+    child_list = get_child_assets_id(child_list, instance_id)
+    all_list = list(Asset.objects.values_list('instance_id', flat=True))
+    possible_parent_list = [a for a in all_list if a not in child_list]
+    return possible_parent_list[fake.pyint(max_value=len(possible_parent_list))]
 
 
 def assets():
@@ -56,6 +68,7 @@ def assets():
 
     for a in Asset.objects.all():
         a.parent_instance_id = get_random_parent_asset(a.instance_id)
+        a.save()
 
 
 def setup():
@@ -63,10 +76,14 @@ def setup():
     Asset(asset_number=fake.name())
 
 
-def test_get_parameter_value():
-    assert get_parameter_value('is_sync_hierarchy') is True
+def test_blabla():
+    al = [10002]
+    print(get_child_assets_id(al, 10002))
 
-
-def test_set_parameter_value():
-    set_parameter_value('test_text_value', 'T', 'test')
-    assert Parameter.objects.get(name='test_text_value').text_value == 'test'
+# def test_get_parameter_value():
+#     assert get_parameter_value('is_sync_hierarchy') is True
+#
+#
+# def test_set_parameter_value():
+#     set_parameter_value('test_text_value', 'T', 'test')
+#     assert Parameter.objects.get(name='test_text_value').text_value == 'test'
